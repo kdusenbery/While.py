00b681672b51bb711cec57a47aa55d97e97aa854
Kohl R Dusenbery <31451970+kdusenbery@users.noreply.github.com>
2017-12-31 04:47:26 -0600

    Add files via upload

---
+++ While.py
@@ -0,0 +1,18 @@
+"""
+Kohl Dusenbery
+ENTD200
+Prof. Edeki
+Week 6
+Aug 12, 2017
+"""
+
+a = 0
+c = 0
+
+while (c<5):
+    b = a + 1
+    c = a + b
+    a += 1
+    print(c)
+
+print ("number of loops until c >= 5:",a)
